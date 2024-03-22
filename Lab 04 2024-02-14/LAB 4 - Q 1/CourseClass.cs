ceb31fac1726143372b647a819c1c28679123b5d
alpthirani <135725809+alpthirani@users.noreply.github.com>
2024-03-22 19:02:07 +0530

    Add files via upload

---
+++ Lab 04 2024-02-14/LAB 4 - Q 1/CourseClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_4___Q_1
+{
+    internal class CourseClass
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter course name:");
+            string courseName = Console.ReadLine();
+
+            Console.Write("Enter instructor name: ");
+            string instructorName = Console.ReadLine();
+
+            Console.WriteLine("Enter grade (0-100): ");
+            double grade = double.Parse(Console.ReadLine());
+
+
+            Course course = new Course(courseName);
+
+            course.SetInstructorName(instructorName);
+            course.Grade = grade;
+
+            course.PrintCourseInfo();
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
+
