d7982929431525fc3a41308f40f8b0a569a3d155
alpthirani <135725809+alpthirani@users.noreply.github.com>
2024-04-01 19:44:21 +0530

    Add files via upload

---
+++ Lab 05 2024-02-21/Lab sheet 03 - Q3/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet_03___Q3
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            // Creating an instance of the Product class
+            Product laptop = new Product(101, "Laptop", 800, 10);
+
+            // Accessing and displaying the name, price, and quantity of the product
+            laptop.DisplayProductInfo();
+
+        }
+    }
+}
